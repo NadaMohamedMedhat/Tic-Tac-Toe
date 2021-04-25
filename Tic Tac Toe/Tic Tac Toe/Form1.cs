aa3d56ea875f6e3097932cd1819fd5cbc2d3e5a6
Mohab Mohsen <54236117+Mohabeldiin@users.noreply.github.com>
2021-04-25 14:12:01 -0700

    Cleaning Code
    optimizing code by add  rest(bool), score_btn(Button,Button,Button,String)  methods
    and editing btn_newgame_Click(),btn_reset_Click(),score() methods

--- Tic Tac Toe/Tic Tac Toe/Form1.cs
+++ Tic Tac Toe/Tic Tac Toe/Form1.cs
@@ -7,227 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace Tic_Tac_Toe
 {
     public partial class Form1 : Form
     {
         Boolean checker;
         int plus_one;
-        void enable_false(){
-            button1.Enabled = false;
-            button2.Enabled = false;
-            button3.Enabled = false;
-            button4.Enabled = false;
-            button5.Enabled = false;
-            button6.Enabled = false;
-            button7.Enabled = false;
-            button8.Enabled = false;
-            button9.Enabled = false;
+        private void enable_false(bool TF){
+            button1.Enabled = TF;
+            button2.Enabled = TF;
+            button3.Enabled = TF;
+            button4.Enabled = TF;
+            button5.Enabled = TF;
+            button6.Enabled = TF;
+            button7.Enabled = TF;
+            button8.Enabled = TF;
+            button9.Enabled = TF;
         }
-        void score()
+        private void score_btn(Button TempButton1, Button TempButton2, Button TempButton3,String Btn_Text)
         {
-              //******************************((horizontal))*******************************************
-                if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X")
-                {
-                    button1.BackColor = Color.PowderBlue;
-                    button2.BackColor = Color.PowderBlue;
-                    button3.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player X");
-                    plus_one = int.Parse(score_X.Text);
-                    score_X.Text = Convert.ToString(plus_one + 1);
-                    enable_false();
-                }
-                if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
-                {
-                    button4.BackColor = Color.PowderBlue;
-                    button5.BackColor = Color.PowderBlue;
-                    button6.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player X");
-                    plus_one = int.Parse(score_X.Text);
-                    score_X.Text = Convert.ToString(plus_one + 1);
-                    enable_false();
-                }
-                if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
-                {
-                    button7.BackColor = Color.PowderBlue;
-                    button8.BackColor = Color.PowderBlue;
-                    button9.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player X");
-                    plus_one = int.Parse(score_X.Text);
-                    score_X.Text = Convert.ToString(plus_one + 1);
-                    enable_false();
-                }
-                //*******************************((vertical ))********************************************
-                if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X")
-                {
-                    button1.BackColor = Color.PowderBlue;
-                    button4.BackColor = Color.PowderBlue;
-                    button7.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player X");
-                    plus_one = int.Parse(score_X.Text);
-                    score_X.Text = Convert.ToString(plus_one + 1);
-                    enable_false();
-                }
-                if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X")
-                {
-                    button2.BackColor = Color.PowderBlue;
-                    button5.BackColor = Color.PowderBlue;
-                    button8.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player X");
-                    plus_one = int.Parse(score_X.Text);
-                    score_X.Text = Convert.ToString(plus_one + 1);
-                    enable_false();
-                }
-                if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X")
-                {
-                    button3.BackColor = Color.PowderBlue;
-                    button6.BackColor = Color.PowderBlue;
-                    button9.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player X");
-                    plus_one = int.Parse(score_X.Text);
-                    score_X.Text = Convert.ToString(plus_one + 1);
-                    enable_false();
-                }
-                //**************************************((diagonal))*************************************
-                if (button1.Text == "X" && button5.Text == "X" && button9.Text == "X")
-                {
-                    button1.BackColor = Color.PowderBlue;
-                    button5.BackColor = Color.PowderBlue;
-                    button9.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player X");
-                    plus_one = int.Parse(score_X.Text);
-                    score_X.Text = Convert.ToString(plus_one + 1);
-                    enable_false();
-                }
-                if (button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
-                {
-                    button3.BackColor = Color.PowderBlue;
-                    button5.BackColor = Color.PowderBlue;
-                    button7.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player X");
-                    plus_one = int.Parse(score_X.Text);
-                    score_X.Text = Convert.ToString(plus_one + 1);
-                    enable_false();
-                }
-
-                //#################### ((For Player O))###############################
-
-                //******************************((horizontal))*******************************************
-                if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O")
-                {
-                    button1.BackColor = Color.PowderBlue;
-                    button2.BackColor = Color.PowderBlue;
-                    button3.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player O");
-                    plus_one = int.Parse(score_O.Text);
-                    score_O.Text = Convert.ToString(plus_one + 1);
-                enable_false();
+            TempButton1.BackColor = Color.PowderBlue;
+            TempButton2 = Color.PowderBlue;
+            TempButton3 = Color.PowderBlue;
+            if (Btn_Text == "X")
+            {
+                MessageBox.Show(" The Winner Is Player X");
+                plus_one = int.Parse(score_X.Text);
+                score_X.Text = Convert.ToString(plus_one + 1);
             }
-                if (button4.Text == "O" && button5.Text == "O" && button6.Text == "O")
-                {
-                    button4.BackColor = Color.PowderBlue;
-                    button5.BackColor = Color.PowderBlue;
-                    button6.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player O");
-                    plus_one = int.Parse(score_O.Text);
-                    score_O.Text = Convert.ToString(plus_one + 1);
-                enable_false();
+            else
+            {
+                MessageBox.Show(" The Winner Is Player O");
+                plus_one = int.Parse(score_O.Text);
+                score_O.Text = Convert.ToString(plus_one + 1);
             }
-                if (button7.Text == "O" && button8.Text == "O" && button9.Text == "O")
-                {
-                    button7.BackColor = Color.PowderBlue;
-                    button8.BackColor = Color.PowderBlue;
-                    button9.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player O");
-                    plus_one = int.Parse(score_O.Text);
-                    score_O.Text = Convert.ToString(plus_one + 1);
-                enable_false();
-            }
-                //********************************((vertical ))********************************************
-                if (button1.Text == "O" && button4.Text == "O" && button7.Text == "O")
-                {
-                    button1.BackColor = Color.PowderBlue;
-                    button4.BackColor = Color.PowderBlue;
-                    button7.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player O");
-                    plus_one = int.Parse(score_O.Text);
-                    score_O.Text = Convert.ToString(plus_one + 1);
-                enable_false();
-            }
-                if (button2.Text == "O" && button5.Text == "O" && button8.Text == "O")
-                {
-                    button2.BackColor = Color.PowderBlue;
-                    button5.BackColor = Color.PowderBlue;
-                    button8.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player O");
-                    plus_one = int.Parse(score_O.Text);
-                    score_O.Text = Convert.ToString(plus_one + 1);
-                enable_false();
-            }
-                if (button3.Text == "O" && button6.Text == "O" && button9.Text == "O")
-                {
-                    button3.BackColor = Color.PowderBlue;
-                    button6.BackColor = Color.PowderBlue;
-                    button9.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player O");
-                    plus_one = int.Parse(score_O.Text);
-                    score_O.Text = Convert.ToString(plus_one + 1);
-                enable_false();
-            }
-                //*******************************((diagonal))********************************************
-                if (button1.Text == "O" && button5.Text == "O" && button9.Text == "O")
-                {
-                    button1.BackColor = Color.PowderBlue;
-                    button5.BackColor = Color.PowderBlue;
-                    button9.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player O");
-                    plus_one = int.Parse(score_O.Text);
-                    score_O.Text = Convert.ToString(plus_one + 1);
-                enable_false();
-            }
-                if (button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
-                {
-                    button3.BackColor = Color.PowderBlue;
-                    button5.BackColor = Color.PowderBlue;
-                    button7.BackColor = Color.PowderBlue;
-                    MessageBox.Show(" The Winner Is Player O");
-                    plus_one = int.Parse(score_O.Text);
-                    score_O.Text = Convert.ToString(plus_one + 1);
-                enable_false();
-            }
+            enable_false(false);
+        }
+        private void score()
+        {
+            //******************************((horizontal))*******************************************
+            if (button1.Text ==  button2.Text && button2.Text == button3.Text ) score_btn(button1, button2, button3, button1.Text);
+            else if (button4.Text == button5.Text && button5.Text== button6.Text ) score_btn(button4, button5, button6, button4.Text);
+            else if (button7.Text == button8.Text && button8.Text== button9.Text) score_btn(button7, button8, button9, button7.Text);
+            //*******************************((vertical ))********************************************
+            else if (button1.Text == button4.Text && button4.Text== button7.Text) score_btn(button1, button2, button7, button1.Text);
+            else if (button2.Text == button5.Text && button5.Text == button8.Text) score_btn(button2, button5, button8, button2.Text);
+            else if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X") score_btn(button3, button6, button9, button3.Text);
+            //**************************************((diagonal))*************************************
+            else if (button1.Text == button5.Text && button5.Text == button9.Text) score_btn(button1, button5, button9, button1.Text);
+            else if (button3.Text == button5.Text && button5.Text == button7.Text) score_btn(button3, button5, button7, button3.Text);
             //*******************************((No_Win))********************************************
-            if (button1.Text != "" && button2.Text != "" && button3.Text != ""
+            else if (button1.Text != "" && button2.Text != "" && button3.Text != ""
                 && button4.Text != "" && button5.Text != "" && button6.Text != ""
                 && button7.Text != "" && button8.Text != "" && button9.Text != "") MessageBox.Show(" No Player Win!");
             }
-            
-        public Form1()
-        {
-            InitializeComponent();
-        }
-        private void panel3_Paint(object sender, PaintEventArgs e) 
-        {
-
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-
+        public Form1() => InitializeComponent();
+        private void panel3_Paint(object sender, PaintEventArgs e) {}
+        private void Form1_Load(object sender, EventArgs e){}
         //*************************((Labels))**********************************
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        private void label1_Click(object sender, EventArgs e) {}
+        private void label4_Click(object sender, EventArgs e) {}
         //*************************((Buttons))**********************************
-               private void TempButton_Click(Button TempButton)
+        private void TempButton_Click(Button TempButton)
         {
             if (checker == false)
             {
@@ -251,20 +92,10 @@ namespace Tic_Tac_Toe
         private void button7_Click(object sender, EventArgs e) => TempButton_Click(button7);
         private void button8_Click(object sender, EventArgs e) => TempButton_Click(button8);
         private void button9_Click(object sender, EventArgs e) => TempButton_Click(button9);
-
         //*******************((New Game & Reset))***************************
-        private void btn_newgame_Click(object sender, EventArgs e)
+        private void rest()
         {
-            button1.Enabled = true;
-            button2.Enabled = true;
-            button3.Enabled = true;
-            button4.Enabled = true;
-            button5.Enabled = true;
-            button6.Enabled = true;
-            button7.Enabled = true;
-            button8.Enabled = true;
-            button9.Enabled = true;
-
+            enable_false(true);
             button1.Text = "";
             button2.Text = "";
             button3.Text = "";
@@ -274,10 +105,6 @@ namespace Tic_Tac_Toe
             button7.Text = "";
             button8.Text = "";
             button9.Text = "";
-
-            score_O .Text= "0";
-            score_X.Text = "0";
-
             button1.BackColor = Color.WhiteSmoke;
             button2.BackColor = Color.WhiteSmoke;
             button3.BackColor = Color.WhiteSmoke;
@@ -287,43 +114,18 @@ namespace Tic_Tac_Toe
             button7.BackColor = Color.WhiteSmoke;
             button8.BackColor = Color.WhiteSmoke;
             button9.BackColor = Color.WhiteSmoke;
-
+        }
+        private void btn_newgame_Click(object sender, EventArgs e)
+        {
+            rest();
+            score_O .Text= "0";
+            score_X.Text = "0";
         }
         private void btn_reset_Click(object sender, EventArgs e)
         {
-            button1.Enabled = true;
-            button2.Enabled = true;
-            button3.Enabled = true;
-            button4.Enabled = true;
-            button5.Enabled = true;
-            button6.Enabled = true;
-            button7.Enabled = true;
-            button8.Enabled = true;
-            button9.Enabled = true;
-
-            button1.Text = "";
-            button2.Text = "";
-            button3.Text = "";
-            button4.Text = "";
-            button5.Text = "";
-            button6.Text = "";
-            button7.Text = "";
-            button8.Text = "";
-            button9.Text = "";
-
+            rest();
+            enable_false(true);
             btn_newgame.Enabled = true;
-
-            button1.BackColor = Color.WhiteSmoke;
-            button2.BackColor = Color.WhiteSmoke;
-            button3.BackColor = Color.WhiteSmoke;
-            button4.BackColor = Color.WhiteSmoke;
-            button5.BackColor = Color.WhiteSmoke;
-            button6.BackColor = Color.WhiteSmoke;
-            button7.BackColor = Color.WhiteSmoke;
-            button8.BackColor = Color.WhiteSmoke;
-            button9.BackColor = Color.WhiteSmoke;
         }
     }
     }
-    
-
